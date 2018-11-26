8f59c99af8da8b7610ff2d8d29bcf86e72f08847
Erin Tait <erinmtait@gmail.com>
2018-11-25 21:50:23 -0800

    finished basic calculator in oop

--- exercises/basiccalcobject.js
+++ exercises/basiccalcobject.js
@@ -1,10 +1,19 @@
 
 class Calculator{
 	constructor(){
+		this.num1 = null;
+		this.operator = null;
+		this.num2 = null;
 		//takes in nothing
 		//make storage for the operator and the numbers
 	}
-	loadOperator(  ){
+	loadOperator(operator){
+		if(operator === '+' || operator === '-' || operator === '*' || operator === '/'){
+			this.operator = operator;
+			return true;
+		} else {
+			return false;
+		}
 		//adds the operator to the next calculation
 		//takes in the operator
 		//checks if it is a valid operation (+-*/)
@@ -12,7 +21,18 @@ class Calculator{
 			//return true
 		//or return false if not the right operator
 	}
-	loadNumber(  ){
+	loadNumber(number){
+		this.number = number;
+
+		if(!isNaN(number) && this.num1 === null){
+			this.num1 = this.number;
+			return 1;
+		} else if(!isNaN(number) && this.num2 === null){
+			this.num2 = this.number;
+			return 2;
+		} else {
+			return false;
+		}
 		//takes in a number and stores it as one of the numbers to perform math on
 		//takes in 1 number
 		//checks if it is actually a number and if we have fewer than 2 numbers
@@ -21,6 +41,20 @@ class Calculator{
 		//otherwise return false (too many numbers stored)
 	}
 	calculate(){
+		if(this.operator === '+'){
+			this.result = this.num1 + this.num2;
+		} else if(this.operator === '-'){
+			this.result = this.num1 - this.num2;
+		} else if(this.operator === '*'){
+			this.result = this.num1 * this.num2;
+		} else if(this.operator === '/'){
+			this.result = this.num1 / this.num2;
+		}
+
+		this.num1 = null;
+		this.num2 = null;
+
+		return this.result;
 		//calculate the result of the stored numbers and operator
 		//takes in nothing
 		//calculates with the operator and 2 numbers
